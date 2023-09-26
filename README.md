7cefbbc71bcd94b7ba23e25dbb7f582fa4928eaf
pcstoyle <pcstoyle@hotmail.com>
2023-09-25 20:12:43 -0600

    finished project

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# svg-logo-maker
-Bootcamp Week 10 Challenge 
+# Title
+
+SVG Logo Maker
+
+## Description
+
+The SVG Logo Maker allows a user to generate a .svg file using prompts to create a logo. The user will decide the logos shape, color, font color and text. 
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+
+
+## Usage
+
+Users will create a logo in a .svg file.
+
+## Credits
+
+
+
+## License 
+MIT
+
+![License: MIT](https://img.shields.io/badge/license-mit-green.svg)
+
+[License: MIT](https://www.mit.edu/~amini/LICENSE.md)
+
+## Tests
+
+
+
+## Questions
--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-
-
 const SHAPES = require('./lib/shapes')
 
-const shapes = new SHAPES();
-
-shapes.run();
-
-//prompt for text up to 3 characters ✅
-//prompt for text color ✅
-//promot for shape ✅
-//propt for shape color ✅
-//generate logo.svg file
-// confirm "generated logo.svg"
 
 
-
-// fs.readFile('data.csv', 'utf8', (error, data) =>
-// error ? console.error(error) : console.log(data)
-// );
-
-// // fs.appendFile('log.txt', `${process.argv[2]}\n`, (err) =>
-// //   err ? console.error(err) : console.log('Commit logged!')
-// // );
-// //generate logo.svg file
-// // confirm "generated logo.svg"
-// fs.writeFile('logo.svg', process.argv[2], (err) =>
-//   err ? console.error(err) : console.log('Generated logo.svg!')
-// );
